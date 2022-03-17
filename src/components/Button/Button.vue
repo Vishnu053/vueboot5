89099c192e49b6698ecf1ad5b2dcad71b8526f69
vishnu053 <vishnunair053@gmail.com>
2022-03-17 23:05:23 +0530

    INitial commit

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { props } from 'vue-property-decorator'
+import { ref } from 'vue'
+const primary = ref(false)
+function returnButtonType() {
+    return {
+        if(primary) { 'primary' };
+        else if(secondary) { 'secondary' };
+        else if(success) { 'success' };
+        else if(danger) { 'danger' };
+        else if(warning) { 'warning' };
+        else if(info) { 'info' };
+        else if(light) { 'light' };
+        else if(dark) { 'dark' };
+        else if(link) { 'link' };
+    }
+}
+</script>
+<template>
+    <button type="button" :class="returnButtonType">Primary</button>
+</template>
